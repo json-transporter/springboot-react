dc2bef73cf4403b51d45c2e1a69ad2ee1abe4292
LeeEunk <dmsrud7817@naver.com>
2024-01-24 15:00:52 +0900

    spring boot - react 연동 후 배포

---
+++ src/main/java/com/example/demo/controller/HelloController.java
@@ -0,0 +1,12 @@
+package com.example.demo.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+    @GetMapping("/api/test")
+    public String hello(){
+        return "Hello, I'm backend";
+    }
+}
